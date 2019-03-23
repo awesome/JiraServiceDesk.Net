2a2400355699127c697f667c564e3f3560607542
SoAwesomeMan <callme@1800AWESO.ME>
2019-03-23 11:51:39 -0700

    fix AppVeyor build error: MSB4044

--- build/build.ps1
+++ build/build.ps1
@@ -11,14 +11,18 @@ foreach ($src in ls $PSScriptRoot\..\src/*) {
 	Write-Output "build: Building & packaging project in $src"
 
     if ($TagVersionNumber -ne $null) {
-        $version = $TagVersionNumber
+        $PackageVersion = $TagVersionNumber
     }
     else {
-        $version = $BuildVersionNumber
+        $PackageVersion = $BuildVersionNumber
     }
 
+    Write-Output "TagVersionNumber: $TagVersionNumber"
+    Write-Output "BuildVersionNumber: $BuildVersionNumber"
+    Write-Output "PackageVersion: $PackageVersion"
+
     & dotnet build -c Release
-    & dotnet pack -c Release --include-symbols -o ..\..\artifacts --no-build /p:PackageVersion=$version
+    & dotnet pack -c Release --include-symbols -o ..\..\artifacts --no-build /p:PackageVersion=$PackageVersion
     if($LASTEXITCODE -ne 0) { exit 1 }    
 
     Pop-Location
